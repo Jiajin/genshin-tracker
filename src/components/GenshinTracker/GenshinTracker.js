8540ebf31f3fcf0b2ddc1bf8791bf23f5e34a4d3
Sim Jiajin <backup_sjj@hotmail.com>
2021-08-18 15:16:38 +0800

    fix chars data, add weekday and incomplete click functionality

--- src/components/GenshinTracker/GenshinTracker.js
+++ src/components/GenshinTracker/GenshinTracker.js
@@ -6,14 +6,41 @@ import GenshinRegion from "./GenshinRegion";
 
 const GenshinTracker = () => {
   //todo: filter by day
+  const weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+  const { currWeekday, setCurrWeekday } = useState(weekdays[0]);
+  const { regionOneData, setRegionOneData } = useState(
+    genshinData.filter(
+      (item) => item.region === "mondsteadt" && item.day.includes(weekdays[0])
+    )
+  );
+  const weekdayClickHandler = (day) => {
+    //console.log(regionOneData);
+    console.log(
+      genshinData.filter(
+        (item) => item.region === "mondsteadt" && item.day.includes("mon")
+      )
+    );
+  };
   const mondsteadt = genshinData.filter(
     (item) => item.region === "mondsteadt" && item.book === "freedom"
   );
   const liyue = genshinData.filter(
     (item) => item.region === "liyue" && item.book === "gold"
   );
+
   return (
     <div>
+      <div className="parent">
+        {weekdays.map((weekday) => (
+          <div
+            key={weekday} //this is not used except prevent compiler warnings
+            className="weekdays"
+            onClick={() => weekdayClickHandler(weekday)}
+          >
+            {weekday}
+          </div>
+        ))}
+      </div>
       <GenshinRegion singleRegion={mondsteadt} />
       <GenshinRegion singleRegion={liyue} />
     </div>
